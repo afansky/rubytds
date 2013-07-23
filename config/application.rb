518ad95738170d07ef84285edd6719cee4af6898
alexey.fansky <alexey.fansky@effective-soft.com>
2013-07-23 00:22:38 -0700

    fixing layout issues

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module Rubytds
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+      "#{html_tag}".html_safe
+    }
   end
 end
