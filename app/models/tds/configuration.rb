07a060b24efe426889871a7440ad4c556fb2adc2
Alexey Fansky <alexey.fansky@effective-soft.com>
2013-09-30 23:31:32 -0700

    validating redirect url

--- app/models/tds/configuration.rb
+++ app/models/tds/configuration.rb
@@ -2,6 +2,7 @@ class TDS::Configuration < ActiveRecord::Base
   has_many :visitors
   attr_accessible :redirect_url, :landing_url
   validates :redirect_url, presence: true
+  validates :redirect_url, :format => {:with => /https?/}
   after_create :set_landing_url
 
   private
